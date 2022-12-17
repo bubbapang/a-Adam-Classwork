e5a020f343ec41280baa38e5f61c4590789f9282
bubbapang <adamtpangelinan@gmail.com>
2022-12-16 16:15:10 -0800

    Adding files of the day, to test them

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,19 @@
+class PolyTreeNode
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent
+
+    end
+
+    #a
+  #b #c
+#d  #e
+
+
+
+end
